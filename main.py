744b365f71b7978e10a878885a73dfddc27ce6d8
Marouane Mokhles <marouanemokhles@yahoo.com>
2022-09-24 15:43:10 +0200

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygsheets
 import pandas as pd
 import aspose.words as aw
+from colorclass import Color
 
 """ Create and edit Word Document's DocProperties based on user input & Google Sheet Data
 
@@ -17,6 +18,10 @@ Prerequisites to use this project:
     - 
 """
 
+# No. of implemented actions
+IMP_ACTIONS = 2
+
+# Project Codes Data Containers
 proj_code_prefixes = []
 proj_code_id_last = {}
 proj_code_client = {}
@@ -24,18 +29,23 @@ proj_code_client = {}
 proj_codes_all = []
 proj_codes_id_counters = {}
 
+# Document Template Titles Data Containers
 template_title_code = {}
 template_title_type = {}
 
-user_choice_type = ""
 
 def main():
-
     # Reading from args txt file
     file = "args.txt"
     f = open(file, "r")
 
     credentials_path = f.readline()
+    credentials_path = credentials_path.replace("\n","")
+    templates_dir = f.readline()
+    templates_dir = templates_dir.replace("\n", "")
+    options = [line for line in f.readlines() if line.strip()]
+    for option in options:
+        option = option.replace("\n", "")
 
     # authorization with GCP
     gc = pygsheets.authorize(service_file=credentials_path)
@@ -60,8 +70,67 @@ def main():
     parse_data_projects(sheet1_matrix)
     parse_data_templates(sheet2_matrix)
 
+    actions_index = len(options)
+    selected_action = prompt_user_entry(options)
+
+    if selected_action > IMP_ACTIONS or selected_action < 1:
+        print(Color("{red}This action is not implemented yet. Please select another action{/red}"))
+        selected_action = prompt_user_entry(options)
+
+    # elif selected_action > actions_index
+    #     print(Color("{red}This action is not implemented yet. Please select another action{/red}"))
+    #     selected_action = prompt_user_entry()
+
+    match selected_action:
+        case 1:
+            create_document_from_template()
+        case 2:
+            edit_existing_document()
+        case 3:
+            create_new_template()
+        case _:
+            print(Color("{red}This action is not implemented yet. Please select another action{/red}"))
+            selected_action = prompt_user_entry(options)
+
+
+def prompt_user_entry(options):
+    # Prompt user to enter an integer corresponding to one of the actions written in the args file
+    slct = -1
+    print(Color("{red}Please select from the list below\n\n{/red}"))
+    for option in options:
+        print(f"{options.index(option)+1}. {option}")
+    slct_option = input("\n\nYour selection: ")
+    try:
+        slct = int(slct_option)
+    except:
+        print(f'Your entry {slct_option} is incorrect. Please enter a number corresponding to an action from the list!'
+              f'\nTry again ...')
+        slct = prompt_user_entry(options)
+        return slct
+    return slct
+
+
+def create_document_from_template():
+    # Copy template and copy data from spreadsheet referencing project if project report/quotation/proposal
+
+    pass
+
+
+def edit_existing_document():
+    # Locate a file and edit its properties
+
+    pass
+
+
+def create_new_template():
+    # Copy Template of templates, create new document using it, using type and give it a new id, then add it to the
+    # templates worksheet
+
+    pass
+
 
 def parse_data_templates(sheet2_matrix):
+    # Parse Template data and populate global doc template data containers
     for line in sheet2_matrix:
         if sheet2_matrix.index(line) == 0:
             print(f"LOG: Line {sheet2_matrix.index(line)} Skipped")
@@ -77,6 +146,7 @@ def parse_data_templates(sheet2_matrix):
 
 
 def parse_data_projects(sheet1_matrix):
+    # Parse Project data and populate global project data containers
     for line in sheet1_matrix:
         if sheet1_matrix.index(line) == 0:
             print(f"LOG: Line {sheet1_matrix.index(line)} Skipped")
